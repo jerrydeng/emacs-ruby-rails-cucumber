4879a634a8eb9b05180b00031f46b045707990cc
Jerry Deng <jerrydeng93@gmail.com>
2011-01-10 13:53:51 -0500

    groovy support

--- init.el
+++ init.el
@@ -126,5 +126,14 @@
 ; try to improve slow performance on windows.
 (setq w32-get-true-file-attributes nil)
 
+;; add groovy support
+;;; turn on syntax hilighting
+(global-font-lock-mode 1)
+
+;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
+(autoload 'groovy-mode "groovy-mode" "Groovy editing mode." t)
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
 (provide 'init)
 ;;; init.el ends here
