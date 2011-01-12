e83a799ed632e69ad0f32131ffdaf23db64f6266
Jerry Deng <jerry@ideacore.com>
2011-01-12 14:17:45 -0500

    add javascript support

--- init.el
+++ init.el
@@ -96,6 +96,99 @@
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
+;; set to full screen
+"Emacs outshines all other editing software in approximately the
+;; same way that the noonday sun does the stars. It is not just bigger
+;; and brighter; it simply makes everything else vanish."
+;; -Neal Stephenson, "In the Beginning was the Command Line"
+
+;; Benchmarking
+(defvar *emacs-load-start* (current-time))
+
+;; Load path etc:
+
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
+(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
+(setq package-user-dir (concat dotfiles-dir "elpa"))
+(setq custom-file (concat dotfiles-dir "custom.el"))
+
+;; These should be loaded on startup rather than autoloaded on demand
+;; since they are likely to be used in every session:
+
+(require 'cl)
+(require 'saveplace)
+(require 'ffap)
+(require 'uniquify)
+(require 'ansi-color)
+(require 'recentf)
+
+;; Load up ELPA, the package manager:
+
+(require 'package)
+(package-initialize)
+;;(require 'starter-kit-elpa)
+
+;; Load up starter kit customizations:
+
+(require 'starter-kit-defuns)
+(require 'starter-kit-bindings)
+(require 'starter-kit-misc)
+(require 'starter-kit-registers)
+(require 'starter-kit-eshell)
+(require 'starter-kit-lisp)
+(require 'starter-kit-ruby)
+(require 'starter-kit-js)
+
+(regen-autoloads)
+(load custom-file 'noerror)
+
+;; You can keep system- or user-specific customizations here:
+
+(setq system-specific-config (concat dotfiles-dir system-name ".el")
+      user-specific-config (concat dotfiles-dir user-login-name ".el"))
+
+(if (file-exists-p system-specific-config) (load system-specific-config))
+(if (file-exists-p user-specific-config) (load user-specific-config))
+
+;; load cucumber mode
+;; Copy files to ~/.emacs.d/elisp/feature-mode and add this to your
+;; .emacs to load the mode
+(add-to-list 'load-path "~/.emacs.d/elisp/feature-mode")
+;; ;; optional configurations
+;; ;; default language if .feature doesn't have "# language: fi"
+;; ;(setq feature-default-language "fi")
+;; ;; point to cucumber languages.yml or gherkin i18n.yml to use
+;; ;; exactly the same localization your cucumber uses
+;; ;(setq feature-default-i18n-file "/path/to/gherkin/gem/i18n.yml")
+;; ;; and load feature-mode
+(require 'feature-mode)
+(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+
+;; Set and load rspec not working below
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(require 'rspec-mode)
+
+;; Set rails minor-mode
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/rails-minor-mode"))
+;;(require 'rails)
+
+;; Set and load php mode
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(load "php-mode")
+(add-to-list 'auto-mode-alist '("\\.php[34]?\\'\\|\\.phtml\\'" . php-mode))
+
+;; Set and load ruby-mode
+(load "ruby-mode")
+(load "inf-ruby")
+(load "ruby-electric")
+
+;; Load yaml mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
 ;; set emacs nav
 (add-to-list 'load-path "~/.emacs.d/elisp/emacs-nav")
 (require 'nav)
@@ -134,6 +227,11 @@
 (autoload 'groovy-mode "groovy-mode" "Groovy editing mode." t)
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+
+;; load javascript mode
+(add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
+(autoload 'javascript-mode "javascript" nil t)
 
 (provide 'init)
 ;;; init.el ends here
