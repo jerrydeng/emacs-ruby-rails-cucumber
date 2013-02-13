1635adcabfb8261e149292901222546b5f2e68cc
Jerry Deng <jerry@ideacore.com>
2013-02-13 17:54:48 -0500

    fix stylus mode

--- init.el
+++ init.el
@@ -231,9 +231,10 @@
 
 ;; jade
 (add-to-list 'load-path "~/.emacs.d/vendor/jade-mode")
+(require 'stylus-mode)
 (require 'sws-mode)
 (require 'jade-mode)    
-(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.styl$" . stylus-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
 
 ;; load jshint
